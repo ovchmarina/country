4bf76a591f78289bcb4712b6f5d0b492c1167779
“ovchmarina” <marinakabakova84@gmail.com>
2020-12-06 10:30:45 +0300

    “сообщение”

---
+++ Contents.swift
@@ -0,0 +1,19 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+
+
+var country = "Russia"
+switch country {
+case "Russia":
+print("RU🇷🇺")
+case "China":
+print("CN🇨🇳")
+case "USA":
+print("US🇺🇸")
+default:
+print("")
+    }
+
